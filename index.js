1583a9e53e945370fd79889c066d8e720e40b4a3
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-05-23 17:52:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImg(imgName, leftPx, bottomPx){
+    let charName = document.createElement('img')
+charName.src = imgName
+charName.style.position = 'fixed'
+charName.style.left = leftPx
+charName.style.bottom = bottomPx
+document.body.append(charName)
+return charName
+}
+
+
+ 
+ function newItem(imgName, leftPx, bottomPx){
+  let charName =  newImg(imgName, leftPx, bottomPx)
+    charName.addEventListener('click',() =>{
+        charName.remove()
+    })
+}
+
+newImg('./assets/green-character.gif','100px','100px')
+newImg('./assets/pine-tree.png','450px','200px')
+newImg('./assets/tree.png','200px','300px')
+newImg('./assets/pillar.png','350px','100px')
+newImg('./assets/crate.png','150px','200px')
+newImg('./assets/well.png','500px','425px')
+
+newItem('./assets/sword.png', '500px', '405px')
+newItem('./assets/shield.png', '165px', '185px')
+newItem('./assets/staff.png', '600px', '100px')
+
+
+
+
+
+
+
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
